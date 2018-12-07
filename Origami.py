a1da2d80d68b82a2c20159da0702873b8ec731e8
Kaboto731 <40328423+Kaboto731@users.noreply.github.com>
2018-12-07 15:24:48 -0800

    Update Origami.py

--- Origami.py
+++ Origami.py
@@ -23,9 +23,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.optimizers import RMSprop
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks imporr, except a few, believed that was the best I can do and some kids made up rumors about me that most of the students believed to be true. I remember, at my lowest point, telling myself "one day" because I knew someday that I will get to show people that I am stronger than how I was portrayed. My senior year I was fully let into the classroom and I received a 4.0 GPA. I then went on to a community college and retained that 4.0 for four semesters. I then got the opportunity to be at the school of my dreams and I am so thankful for the class I am with. Even though I made it this far, it's still an uphill battle in which my grades are not reflecting me and I push myself to learn and excel in the classrom. I just want to show that if I make this far, then anyone else who is on the spectrum can too.  Never give up on yourself.
-
-t ReduceLROnPlateau
+from keras.callbacks import ReduceLROnPlateau
 #input Height and Width of desired testing image
 H = 28
 W = 28
